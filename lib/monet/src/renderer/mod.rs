a60e9a96ba629a7afb416f46c427543fab42aa90
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2017-06-13 10:01:31 +0200

    Comment renderable trade so build works and update gitignore

--- lib/monet/src/renderer/mod.rs
+++ lib/monet/src/renderer/mod.rs
@@ -93,10 +93,10 @@ impl Renderer {
     }
 }
 
-pub trait Renderable {
-    pub fn setup_in_scene(&mut self, renderer_id: RendererID, scene_id: usize, world: &mut World);
-    pub fn render_to_scene(&mut self, renderer_id: RendererID, scene_id: usize, world: &mut World);
-}
+// pub trait Renderable {
+//     pub fn setup_in_scene(&mut self, renderer_id: RendererID, scene_id: usize, world: &mut World);
+//     pub fn render_to_scene(&mut self, renderer_id: RendererID, scene_id: usize, world: &mut World);
+// }
 
 pub fn setup(system: &mut ActorSystem, initial: Renderer) {
     auto_setup(system, (initial,));
