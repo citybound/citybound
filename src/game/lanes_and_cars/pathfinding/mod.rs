a5d5bb94fd4c4e4bf06458fbae1e37e220efdb50
Vladimir Shakhov <vladimir@spotify.com>
2017-07-22 21:14:47 +0200

    removed debug output

--- src/game/lanes_and_cars/pathfinding/mod.rs
+++ src/game/lanes_and_cars/pathfinding/mod.rs
@@ -318,7 +318,6 @@ pub fn setup(system: &mut ActorSystem) {
                             .map(|&RoutingInfo { distance, .. }| new_distance < distance)
                             .unwrap_or(true);
                         if insert {
-                            println!("path finding {:?}", lane.pathfinding.routes.display());
                             lane.pathfinding.routes.insert(destination,
                                                            RoutingInfo {
                                                                distance: new_distance,
