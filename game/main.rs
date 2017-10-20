7201c374338f4ac76d34a23504c79002d1b58364
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2017-10-20 15:08:34 +0200

    Fix windows stackoverflow (once again) - fixes #202

--- game/main.rs
+++ game/main.rs
@@ -47,77 +47,81 @@ use economy::households::tasks::TaskEndSchedulerID;
 use economy::buildings::rendering::BuildingRendererID;
 
 fn main() {
-    core::init::first_time_open_wiki_release_page();
+    let dummy_thread = ::std::thread::Builder::new().stack_size(32 * 1024 * 1024).spawn(move || {
+        core::init::first_time_open_wiki_release_page();
 
-    let mut system = kay::ActorSystem::new(
-        core::init::create_init_callback(),
-        core::init::networking_from_env_args(),
-    );
+        let mut system = Box::new(kay::ActorSystem::new(
+            core::init::create_init_callback(),
+            core::init::networking_from_env_args(),
+        ));
 
-    let world = &mut system.world();
+        let world = &mut system.world();
 
-    system.networking_connect();
+        system.networking_connect();
 
-    let simulatables = vec![
-        LaneID::local_broadcast(world).into(),
-        TransferLaneID::local_broadcast(world).into(),
-        FamilyID::local_broadcast(world).into(),
-        TaskEndSchedulerID::local_first(world).into(),
-    ].into();
-    let simulation = core::simulation::setup(&mut system, simulatables);
+        let simulatables = vec![
+            LaneID::local_broadcast(world).into(),
+            TransferLaneID::local_broadcast(world).into(),
+            FamilyID::local_broadcast(world).into(),
+            TaskEndSchedulerID::local_first(world).into(),
+        ].into();
+        let simulation = core::simulation::setup(&mut system, simulatables);
 
-    let renderables: CVec<_> = vec![
-        LaneRendererID::global_broadcast(world).into(),
-        GrouperID::global_broadcast(world).into(),
-        CurrentPlanID::global_broadcast(world).into(),
-        BuildingRendererID::global_broadcast(&mut system.world()).into(),
-    ].into();
+        let renderables: CVec<_> = vec![
+            LaneRendererID::global_broadcast(world).into(),
+            GrouperID::global_broadcast(world).into(),
+            CurrentPlanID::global_broadcast(world).into(),
+            BuildingRendererID::global_broadcast(&mut system.world()).into(),
+        ].into();
 
-    let machine_id = system.networking_machine_id();
+        let machine_id = system.networking_machine_id();
 
-    let (user_interface, renderer) = stagemaster::setup(
-        &mut system,
-        renderables,
-        *ENV,
-        core::init::build_window(machine_id),
-        (0.6, 0.75, 0.4, 1.0)
-    );
+        let (user_interface, renderer) = stagemaster::setup(
+            &mut system,
+            renderables,
+            *ENV,
+            core::init::build_window(machine_id),
+            (0.6, 0.75, 0.4, 1.0)
+        );
 
-    transport::setup(&mut system, user_interface, renderer, simulation);
-    economy::setup(&mut system, user_interface, simulation);
+        transport::setup(&mut system, user_interface, renderer, simulation);
+        economy::setup(&mut system, user_interface, simulation);
 
-    core::init::print_version(user_interface, world);
-
-    system.process_all_messages();
-
-    let mut frame_counter = core::init::FrameCounter::new();
-
-    loop {
-        frame_counter.start_frame();
-        frame_counter.print_fps(user_interface, world);
-
-        core::init::print_instance_counts(&mut system, user_interface);
-        core::init::print_network_turn(&mut system, user_interface);
-
-        user_interface.process_events(world);
+        core::init::print_version(user_interface, world);
 
         system.process_all_messages();
 
-        simulation.do_tick(world);
+        let mut frame_counter = core::init::FrameCounter::new();
 
-        system.process_all_messages();
+        loop {
+            frame_counter.start_frame();
+            frame_counter.print_fps(user_interface, world);
 
-        renderer.render(world);
+            core::init::print_instance_counts(&mut system, user_interface);
+            core::init::print_network_turn(&mut system, user_interface);
 
-        system.process_all_messages();
+            user_interface.process_events(world);
 
-        system.networking_send_and_receive();
-        system.process_all_messages();
+            system.process_all_messages();
 
-        user_interface.start_frame(world);
+            simulation.do_tick(world);
 
-        system.process_all_messages();
+            system.process_all_messages();
 
-        system.networking_finish_turn();
-    }
+            renderer.render(world);
+
+            system.process_all_messages();
+
+            system.networking_send_and_receive();
+            system.process_all_messages();
+
+            user_interface.start_frame(world);
+
+            system.process_all_messages();
+
+            system.networking_finish_turn();
+        }
+    }).unwrap();
+
+    dummy_thread.join().unwrap();
 }
